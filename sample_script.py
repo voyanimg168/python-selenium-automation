a0486e667719fa8b99c33e964bc0a360a50f85b3
Min-Ru G <minrugan@gmail.com>
2025-02-22 16:51:37 -0700

    Update automated search script

--- sample_script.py
+++ sample_script.py
@@ -18,7 +18,7 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Table')
+search.send_keys('table')
 
 # wait for 4 sec
 sleep(4)
