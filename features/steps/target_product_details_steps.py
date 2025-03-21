368241601c9b44ca8fb7ab3cc4910afa2b8ebf99
Minru Gan <minrugan@gmail.com>
2025-03-20 22:04:28 -0600

    Scenario outline created to click through color options for multiple products

--- features/steps/target_product_details_steps.py
+++ features/steps/target_product_details_steps.py
@@ -1,61 +1,33 @@
-from selenium.webdriver.common.by import By
-#from selenium.webdriver.support import expected_conditions as EC
-from behave import given, when, then
 from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from behave import given, then
 
-COLOR_OPTIONS = (By.CSS_SELECTOR, "[class='styles_ndsCarouselItem__dnUkr']") # [0:3:1]
-SELECTED_COLOR = (By.CSS_SELECTOR, "[data-test='@web/VariationComponent'] div")
+COLOR_OPTIONS = (By.CSS_SELECTOR, "div[aria-label='Carousel'] li img")
+#COLOR_OPTIONS = (By.CSS_SELECTOR, "[class='styles_fadeInPicture__X_1Zl styles_animated__7ypei styles_fullWidth__z04mu styles_fullHeight__xxwBI'][alt]")
+#ACTUAL_COLOR = ("[data-test='@web/VariationComponent'] div")
+ACTUAL_COLOR = (By.CSS_SELECTOR, 'div[aria-label*="olor"]')
 
 @given('Open target {product_id} page')
 def open_target_product_page(context, product_id):
-    context.driver.get(f'https://www.target.com/p/denizen-from-levi-s-men-s-285-relaxed-fit-jeans/-/A-54551690?preselect=81782659#lnk=sametab')
-    sleep(5)
+    context.driver.get(f'https://www.target.com/p/{product_id}')
 
-@when('Click on {product_id}} color options')
-def click_product_color_options(context, product_id):
-    context.driver.find_elements(*COLOR_OPTIONS).send_keys()
-    sleep(10)
+@then('Click on {product_id} for {expected_colors} options')
+def click_through_colors(context, product_id, expected_colors):
+    sleep(15)
 
-@then('A54551690 color should change')
-def click_through_colors(context):
-    expected_colors = ['Blue Tint', 'Denim Blue', 'Marine', 'Raven']
-    actual_colors = []
-
-    color_options = context.driver.find_elements(*COLOR_OPTIONS).send_keys()
-    print(color_options)
-
-    for color_options in color_options:
-        color_options.send_keys()
-        print(color_options)
-        color_options.find_elements(*COLOR_OPTIONS).send_keys()
-
-        selected_color = context.driver.find_element(*SELECTED_COLOR).send_keys()
-        print('Current color: ', selected_color)
-
-        selected_color = selected_color.split('\n')[1]
-        actual_colors.append(selected_color)
-        print(actual_colors)
-
-    assert actual_colors == expected_colors, f'Expected {expected_colors}, but got {actual_colors}'
-
-@then('A-91511634 color should change')
-def click_through_colors(context):
-    expected_colors = ['black/gum', 'dark khaki', 'grey', 'navy/tan', 'white/navy/red', 'white/sand/tan']
+    expected_colors = expected_colors.split(', ')
     actual_colors = []
 
     color_options = context.driver.find_elements(*COLOR_OPTIONS)
-    print(color_options)
 
-    for color_options in color_options:
-        color_options.send_keys()
-        print(color_options)
-        color_options.find_elements(*COLOR_OPTIONS).send_keys()
+    for color in color_options:
+        sleep(2)
+        color.click()
+        color_name = context.driver.find_element(*ACTUAL_COLOR).text
 
-        selected_color = context.driver.find_element(*SELECTED_COLOR).send_keys()
-        print('Current color: ', selected_color)
+        color_name = color_name.split('\n')[1]
+        actual_colors.append(color_name)
 
-        selected_color = selected_color.split('\n')[1]
-        actual_colors.append(selected_color)
-        print(actual_colors)
+    assert actual_colors == expected_colors, f'Error. Expected {expected_colors}, but got {actual_colors}'
 
-        assert actual_colors == expected_colors, f'Expected {expected_colors}, but got {actual_colors}'
--- features/tests/target_product_details.feature
+++ features/tests/target_product_details.feature
@@ -1,11 +1,10 @@
 # Created by jackd at 3/18/2025
 Feature: Product Page Functionality test cases
 
-  Scenario Outline: Click on <product_id> color options
+  Scenario Outline: Click on color options
     Given Open target <product_id> page
-    When Click on <product_id> color options
-    Then <product_id> color should change
+    Then Click on <product_id> for <expected_colors> options
     Examples:
-    |product_id|
-    |A-54551690 |
-    |A-91511634 |
+      | product_id | expected_colors                                                       |
+      | A-54551690 | Blue Tint, Denim Blue, Marine, Raven                                  |
+      | A-91511634 | black/gum, dark khaki, grey, navy/tan, white/navy/red, white/sand/tan |
