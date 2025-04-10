d6db6122ba8bafb3637ee4f025dc9ad45db405cc
Minru Gan <minrugan@gmail.com>
2025-04-09 23:42:37 -0600

    update help feature page steps

--- features/steps/target_help_steps.py
+++ features/steps/target_help_steps.py
@@ -1,17 +1,39 @@
 from behave import given, when, then
 from selenium.webdriver.common.by import By
 
+CURRENT_PROMOTIONS = (By.CSS_SELECTOR, "[class='boxSmall txtAC commonLinks']")
+HELP_URL = 'https://help.target.com/help'
 HELP_BTN = (By.CSS_SELECTOR, "[class='custom-h2']")
 SEARCH_FIELD = (By.ID, 'search')
 SEARCH_BTN = (By.XPATH, "//button[@class='btn-sm search-btn']")
-TOP_HELP_SECTION = (By.CSS_SELECTOR, "[class='container clearfix'][3]")
-BOTTOM_HELP_SECTION = (By.CSS_SELECTOR, "[class='container clearfix'][4]")
 BROWSE_ALL_HELP = (By.CSS_SELECTOR, "[class='container clearfix'][5]")
+HEADER = (By.XPATH, "//h1[text()=' {SUBSTRING}']")
+HELP_TOPIC_SELECTION_DD = (By.CSS_SELECTOR, "select[id*='ViewHelpTopics']")
 
-@given('Open target help page')
+@given('Open Help page')
 def open_help_page(context):
     # context.driver.get('https://help.target.com/help')
     context.app.help_page.open_help_page()
+
+@given('Open Help Returns page')
+def open_help_returns_page(context):
+    context.app.help_page.open_help_returns_page()
+
+# @then('Verify Help Returns page opened')
+# def verify_returns_opened(context):
+#     context.app.help_page.verify_returns_opened()
+#
+# @then('Verify Help Current Promotions page opened')
+# def verify_promotions_opened(context):
+#     context.app.help_page.verify_promotions_opened()
+
+@when('Select Browse Help topic {option_value}')
+def select_browse_help_topic(context, option_value):
+    context.app.help_page.select_browse_help_topic(option_value)
+
+@then('Verify Help topic {option_value} page opens')
+def verify_help_topic_page_opens(context, option_value):
+    context.app.help_page.verify_help_topic_page_opens(option_value)
 
 @when('Click on Target Help button')
 def click_on_target_help_btn(context):
@@ -28,36 +50,6 @@ def click_on_search_button(context):
     # context.driver.find_element(*SEARCH_BTN).click()
     context.app.help_page.click_search_button(SEARCH_BTN).click()
 
-@given('Open Help page for Returns')
-def click_cart(context):
-    context.app.help_page.open_help_returns()
-
-@when('Select Help topic {option_value}')
-def select_topic(context, option_value):
-    context.app.help_page.select_topic(option_value)
-
-# @then('Verify Help Returns page opened')
-# def verify_returns_opened(context):
-#     context.app.help_page.verify_returns_opened()
-#
-# @then('Verify Help Current Promotions page opened')
-# def verify_promotions_opened(context):
-#     context.app.help_page.verify_promotions_opened()
-
-@then('Verify Help {header} page opened')
-def verify_correct_help_page_opened(context, header):
-    context.app.help_page.verify_help_page_opened(header)
-
-@when('Click on Top Help section')
-def click_on_top_help(context):
-    # context.driver.find_elements(*TOP_HELP_SECTION).click()
-    context.app.help_page.click_on_top_help(TOP_HELP_SECTION).click()
-
-@when('Click on Bottom Help section')
-def click_on_bottom_help(context):
-    # context.driver.find_elements(*BOTTOM_HELP_SECTION).click()
-    context.app.help_page.click_on_bottom_help(BOTTOM_HELP_SECTION).click()
-
 @when('Click on Browse All Help pages')
 def click_on_browse_all_help(context):
     # context.driver.find_element(*BROWSE_ALL_HELP).click()
@@ -69,7 +61,7 @@ def verify_all_ui_element_shown(context, element_amount):
     # elements = context.driver.find_elements(By.CSS_SELECTOR, "[class='container clearfix']")
     # assert element_amount == len(elements), \
     #     f'Expected {element_amount} links, but got {len(elements)}'
-    context.app.verify_all_ui_elements(element_amount)
+    context.app.help_page.verify_all_ui_element_shown(element_amount)
     print('test case passed')
 
 
--- features/tests/target_help.feature
+++ features/tests/target_help.feature
@@ -2,19 +2,15 @@
 Feature: Help Page UI Test Case
 
   Scenario: Verify 6 UI element on the Target Help page
-    Given Open target help page
+    Given Open Help page
     Then Verify 6 UI elements on the Target Help page
 
-  Scenario: User can select Help topic Promotions & Coupons
-    Given Open Help page
-    And Click on View Current Promotions
-    And Verify Help - Current Promotions page opens
-    When Select Browse Help dropdown topic Promotions & Coupons
-    Then Verify Help Current Promotions page opened
+  Scenario Outline: User can select Help topic page
+    Given Open Help Returns page
+    When Select Browse Help topic <option_value>
+    Then Verify Help topic <header> page opens
+    Examples:
+    |option_value         |header               |
+    |Promotions & Coupons |Current promotions   |
+    |Target Circle™       |About Target Circle  |
 
-   Scenario: User can select Help topic Target Circle
-     Given Open Help page
-     And Click on View Current Promotions
-     And Verify Help - Current Promotions page opens
-     When Select Browse Help dropdown topic Target Circle
-     Then Verify Help About Target Circle page opened
